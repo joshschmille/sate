0c50bc3d7507f22084efc1c8448efaf5a0ab3465
Josh Schmille <josh.schmille@gmail.com>
2021-04-03 22:28:52 -0500

    update main.go

--- main.go
+++ main.go
@@ -146,7 +146,7 @@ func main() {
 			termWidth = payload.Width
 			termHeight = payload.Height
 			gameLog.SetRect(0, 0, payload.Width-40, payload.Height-3)
-			statBlock.SetRect(payload.Width-40, 0, payload.Width, 9)
+			statBlock.SetRect(payload.Width-40, 0, payload.Width, 11)
 			missionBlock.SetRect(payload.Width-40, 11, payload.Width, 21)
 
 			startX, startY, endX, endY := calculateMacguffinRect()
